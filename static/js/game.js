f5b7ef7b54bdbcfe3746abcdeea06627dd55a3aa
Jerzy Andrusiewicz <jurek.andrusiewicz@gmail.com>
2025-05-24 00:46:22 +0200

    disclaimer to js file

--- static/js/game.js
+++ static/js/game.js
@@ -1,3 +1,6 @@
+// FILE DEDICATED TO SCRIPTS IN GAME_PAGE
+// (NOT TO BE MISTAKEN WITH FLAPPY BIRD FILES)
+
 // navigation to challenge page
 $(document).ready(function() {
     $('#back-button').on('click', function() {
