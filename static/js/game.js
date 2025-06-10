7f7acfe59bd994ec0af532da3ff7997afa9a6faa
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2025-06-10 19:23:24 +0200

    Changing skins and background code

--- static/js/game.js
+++ static/js/game.js
@@ -8,3 +8,57 @@ $(document).ready(function() {
     });
 });
 
+// Zmienne globalne
+let currentBackgroundIndex = 0;
+
+// Funkcja do zmiany tła
+function updateBackgroundPreview() {
+    const backgroundPreview = document.getElementById('game-background-preview');
+    backgroundPreview.src = backgrounds[currentBackgroundIndex];
+}
+
+// Obsługa przycisków strzałek
+document.getElementById('background-arrow-left').addEventListener('click', () => {
+    currentBackgroundIndex = (currentBackgroundIndex - 1 + backgrounds.length) % backgrounds.length;
+    updateBackgroundPreview();
+});
+
+document.getElementById('background-arrow-right').addEventListener('click', () => {
+    currentBackgroundIndex = (currentBackgroundIndex + 1) % backgrounds.length;
+    updateBackgroundPreview();
+});
+
+// Obsługa przycisku "POTWIERDŹ"
+document.querySelector('#game-eq-footer .game-styled-button').addEventListener('click', () => {
+    alert(`Wybrano tło: ${backgrounds[currentBackgroundIndex]}`);
+    // Możesz tutaj wysłać dane do backendu, np.:
+    // fetch('/save-background', { method: 'POST', body: JSON.stringify({ background: backgrounds[currentBackgroundIndex] }) });
+});
+
+
+// Zmienne globalne
+let currentSkinIndex = 0;
+
+// Funkcja do zmiany podglądu skórki
+function updateSkinPreview() {
+    const skinPreview = document.getElementById('game-skin-preview');
+    skinPreview.src = skins[currentSkinIndex];
+}
+
+// Obsługa przycisków strzałek dla skórek
+document.getElementById('skin-arrow-left').addEventListener('click', () => {
+    currentSkinIndex = (currentSkinIndex - 1 + skins.length) % skins.length;
+    updateSkinPreview();
+});
+
+document.getElementById('skin-arrow-right').addEventListener('click', () => {
+    currentSkinIndex = (currentSkinIndex + 1) % skins.length;
+    updateSkinPreview();
+});
+
+// Obsługa przycisku "POTWIERDŹ" dla skórek
+document.querySelector('#game-eq-footer .game-styled-button').addEventListener('click', () => {
+    alert(`Wybrano skórkę: ${skins[currentSkinIndex]}`);
+    // Możesz tutaj wysłać dane do backendu, np.:
+    // fetch('/save-skin', { method: 'POST', body: JSON.stringify({ skin: skins[currentSkinIndex] }) });
+});
